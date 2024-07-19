d55782fce65ee716a8d3d3c593c8a716da41b3a3
Onigiriyama <curfews.hinge.00@icloud.com>
2024-07-19 18:05:17 +0900

    ユーザー管理機能修正3回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' }
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width katakana characters' }
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
   validates :last_name_kana, presence: true,
-                             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width katakana characters' }
+                             format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
   validates :birth_day, presence: true
 end
