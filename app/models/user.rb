05f65c04b07419182317ff287018a0524bbd3d3f
Onigiriyama <curfews.hinge.00@icloud.com>
2024-07-26 14:59:02 +0900

    Userバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
+    validates :email, uniqueness: true
+    validates :encrypted_password
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
     validates :birth_day
   end
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers' }
 end
